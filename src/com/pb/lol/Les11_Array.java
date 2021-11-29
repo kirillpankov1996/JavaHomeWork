ce0b200e98c813e33a8931fdd86a0a36c4923c20
kirillpankov <kirill.pankov@privatbank.ua>
2021-11-29 23:29:10 +0200

    training

--- src/com/pb/lol/Les11_Array.java
+++ src/com/pb/lol/Les11_Array.java
@@ -1,34 +1,50 @@
 package com.pb.lol;
+
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Les11_Array {
-    public static void main (String [] args) {
+    public static void main(String[] args) {
+        int[] array = new int[10];
+        int sumAllElements = 0;
+        int countPositiveElements = 0;
+        int sortArray;
+        boolean sortState = false;
+
+
         Scanner scanner = new Scanner(System.in);
-        int [] array = new int [10];
-        for(int i=0; i<array.length; i++){
-            System.out.println("Введите %d-e число массива", (i + 1));
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.printf("Введите %d-e число массива: ", (i + 1));
             array[i] = scanner.nextInt();
+
+            if (array[i] > 0) {
+                countPositiveElements++;
+            }
+            sumAllElements += array[i];
         }
 
+        System.out.println("Вы ввели массивы из чисел: " + Arrays.toString(array));
+        System.out.println("Сумма всех элементов массива: " + sumAllElements);
+        System.out.println("Количество положительных элементов массива: " + countPositiveElements);
+
+        while (!sortState) {
+            sortState = true;
+            for (int i = 0; i < (array.length - 1); i++) {
+                if (array[i] > array[i + 1]) {
+                    sortState = false;
+                    sortArray = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = sortArray;
+
+                }
+
+            }
+
+        }
+        System.out.println("Отсортированный массив: " + Arrays.toString(array));
     }
+
 }
 
 
-
-
-
-
-
-/*
-Массив 10 эллементов
-Сумма элементов
-2. Создайте класс Array в пакете hw3.
-Программа должна позволить пользователю ввести одномерный массив целых чисел размерностью 10 элементов.
-        Вывести на экран введенный массив.
-        Подсчитать сумму всех элементов массива и вывести ее на экран.
-        Подсчитать и вывести на экран количество положительных элементов.
-        Произвести сортировку этого массива от меньшего к большему по алгоритму сортировки пузырьком.
-        (https://en.wikipedia.org/wiki/Bubble_sort)
-        Вывести на экран отсортированный массив
- */
