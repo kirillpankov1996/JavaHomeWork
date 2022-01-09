01bd1ec980c71d0c1fec30ba3124a465e96fce3f
kirillpankov <kirill.pankov@privatbank.ua>
2022-01-09 18:54:08 +0200

    HW 14

---
+++ src/com/pb/pankov/hw14/Client.java
@@ -0,0 +1,87 @@
+package com.pb.pankov.hw14;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.net.Socket;
+
+public class Client extends JFrame implements ActionListener {
+    private static final int PORT = Server.PORT;
+    private static final int WIDTH = 600;
+    private static final int HEIGHT = 400;
+
+    public static void main(String[] args) throws IOException {
+        SwingUtilities.invokeLater(Client::new);
+    }
+
+    private String name;
+    BufferedReader server;
+    PrintWriter writer;
+    private final JTextArea log = new JTextArea();
+    private final JTextField input = new JTextField();
+
+    private Client() {
+        try {
+            Socket socket = new Socket("localhost", PORT);
+            server = new BufferedReader(new InputStreamReader(socket.getInputStream(), Server.UTF8));
+            writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), Server.UTF8), true);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(WIDTH, HEIGHT);
+        setLocationRelativeTo(null);
+        setAlwaysOnTop(true);
+
+        log.setEditable(false);
+        log.setLineWrap(true);
+        log.setText("введіть ваше ім'я\n");
+        add(log, BorderLayout.CENTER);
+
+        input.addActionListener(this);
+        add(input, BorderLayout.SOUTH);
+
+        setVisible(true);
+        new Thread(this::listenMessage).start();
+    }
+
+    private void listenMessage() {
+        String message;
+        try {
+            while ((message = server.readLine()) != null) {
+                print(message);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void print(String message) {
+        SwingUtilities.invokeLater(() -> {
+            log.append(message + "\n");
+            log.setCaretPosition(log.getDocument().getLength());
+        });
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String message = input.getText();
+
+        if (name == null) {
+            name = (message.length() != 0) ? message : "anonim";
+            setTitle(name);
+            message = name;
+            log.setText("введіть повідомлення\n");
+        } else {
+            message = input.getText();
+            if (message.equals("")) return;
+        }
+        writer.println(message);
+        input.setText("");
+    }
+}
---
+++ src/com/pb/pankov/hw14/Server.java
@@ -0,0 +1,71 @@
+package com.pb.pankov.hw14;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+    public static final int PORT = 25000;
+    public static final Charset UTF8 = StandardCharsets.UTF_8;
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(PORT);
+        ExecutorService threadPool = Executors.newFixedThreadPool(10);
+        threadPool.submit(new Sender());
+
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            Receiver receiver = new Receiver(clientSocket);
+            Sender.receivers.add(receiver);
+            threadPool.submit(receiver);
+        }
+    }
+
+    static class Receiver implements Runnable {
+        static private final ConcurrentLinkedQueue<String> messages = new ConcurrentLinkedQueue<>();
+
+        private final String clientName;
+        private final BufferedReader reader;
+        private final PrintWriter writer;
+
+        public Receiver(Socket socket) throws IOException {
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), UTF8));
+            clientName = reader.readLine();
+            writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), UTF8), true);
+        }
+
+        @Override
+        public void run() {
+            try {
+                String message;
+                while ((message = reader.readLine()) != null) {
+                    messages.add(String.format("%s - %s  ----  %s", clientName, LocalDateTime.now(), message));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    static class Sender implements Runnable {
+        private static final List<Receiver> receivers = new ArrayList<>();
+
+        @Override
+        public void run() {
+            while (true) {
+                if (!Receiver.messages.isEmpty()) {
+                    String message = Receiver.messages.remove();
+                    receivers.forEach(receiver -> receiver.writer.println(message));
+                }
+            }
+        }
+    }
+}
